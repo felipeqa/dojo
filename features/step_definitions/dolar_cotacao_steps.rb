af3f8a25fba3707ebf9047cfa5aa8619d1cd1b85
Felipe Rodrigues <Felipe Rodrigues>
2017-04-20 00:16:26 -0300

    Dojo noite

--- features/step_definitions/dolar_cotacao_steps.rb
+++ features/step_definitions/dolar_cotacao_steps.rb
@@ -1,23 +1,30 @@
 Dado(/^que eu estou em um site de busca$/) do
-  visit 'https://br.yahoo.com/?p=us'
+  visit 'https://www.bing.com/?cc=br'
 end
 
 Dado(/^busco por UOL o melhor conteudo$/) do
-  find('#uh-search-box').set('uol o melhor conteudo')
-  find('#uh-search-button').click
+  find('#sb_form_q').set('uol melhor conteudo')
+  find('#sb_form_go').click
   sleep 3
+  click_link('UOL - O melhor conteúdo', match: :first) 
+  sleep 5
 end
 
 Quando(/^acesso a pagina de economia$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  find(:css, '#menuHorizontal > div > div > ul > li.menu-uol-item.area-economia > a').click
+  sleep 3
 end
 
 Quando(/^busco a cotação do dolar$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+   find(:xpath,'//*[@id="header-menu"]/ul/li[2]/a').hover
+   sleep 3
+   expect(page).to have_selector(:xpath,'//*[@id="header-menu"]/ul/li[2]/div/ul/li[3]/a', visible: true)
+   sleep 3
+   find(:xpath,'//*[@id="header-menu"]/ul/li[2]/div/ul/li[3]/a').click
+   sleep 3
+
 end
 
 Então(/^comparo se o valor é menor que "([^"]*)"$/) do |arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
-
-id="uh-search-box"
